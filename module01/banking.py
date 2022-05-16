a1c4452ea2b472605e0970697745334fee85d9bb
deepcloudlabs <deepcloudlabs@gmail.com>
2022-05-16 11:57:00 +0300

    Update banking.py

--- module01/banking.py
+++ module01/banking.py
@@ -1,3 +1,9 @@
+class InsufficientBalanceError(Exception):  # inheritance
+    def __init__(self, message, deficit):
+        self.message = message
+        self.deficit = deficit
+
+
 class Account:
     def __init__(self, iban, balance=0):
         self._iban = iban
@@ -22,3 +28,7 @@ class Account:
         self._balance = self._balance - amount
 
     def deposit(self, amount):
+        # validation
+        if amount <= 0:
+            raise ValueError("deposit amount must be positive.")
+        self._balance = self._balance + amount
