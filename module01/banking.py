39fcf6a2f73d611621d124338e5d8abaa84d0fef
deepcloudlabs <deepcloudlabs@gmail.com>
2022-05-16 12:22:23 +0300

    Update banking.py

--- module01/banking.py
+++ module01/banking.py
@@ -32,3 +32,32 @@ class Account:
         if amount <= 0:
             raise ValueError("deposit amount must be positive.")
         self._balance = self._balance + amount
+
+    def __str__(self):
+        return f"Account [iban: {self._iban}, balance: {self._balance}]"
+
+
+class CheckingAccount(Account):
+    """
+    CheckingAccount : Derived Class, Sub-class
+    Account         : Base Class, Super-class
+    """
+
+    def __init__(self, iban, balance, overdraft_amount=500):
+        super().__init__(iban, balance)
+        self._overdraft_amount = overdraft_amount
+
+    @property
+    def overdraft_amount(self):
+        return self._overdraft_amount
+
+    # overriding
+    def withdraw(self, amount):
+        # validation
+        if amount <= 0:
+            raise ValueError("withdraw amount must be positive.")
+        # business rule
+        if amount > (self._balance + self._overdraft_amount):
+            deficit = amount - self._balance - self._overdraft_amount
+            raise InsufficientBalanceError("your balance does not cover your expenses", deficit)
+        self._balance = self._balance - amount
