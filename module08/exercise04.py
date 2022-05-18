904fb73ed855bc575fc132d1e18c936c810002f1
deepcloudlabs <deepcloudlabs@gmail.com>
2022-05-18 18:34:53 +0300

    initial commit

--- module08/exercise04.py
+++ module08/exercise04.py
@@ -11,5 +11,6 @@ employees = hr_db.employees  # employees collection
 """
     http://binkurt.blogspot.com/2015/02/mongodb-ile-calsmak.html
 """
-for emp in employees.find({"$and" : [ {"birth_year": {"$gte": 1960, "$lt": 1970}}, {"salary" : { "$gte": 75000}}] }).sort("fullname", 1):
+for emp in employees.find({"$and": [{"birth_year": {"$gte": 1960, "$lt": 2000}}, {"salary": {"$gte": 75000}}]}).sort(
+        "fullname", 1):
     pp(emp)
